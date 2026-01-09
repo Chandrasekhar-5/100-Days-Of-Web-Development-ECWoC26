e02bb0e49c1695715f8aad40e7e13a7affa26bd3
Varri Chandrasekhar <varrichandrasekhar5@gmail.com>
2026-01-09 15:34:56 +0530

    generate cards dynamically

---
+++ public/Day 31/script.js
@@ -0,0 +1,48 @@
+const jobs = [
+    {
+        title: "Frontend Developer",
+        company: "ABC Tech",
+        location: "Remote",
+        type: "Full Time"
+    },
+    {
+        title: "Backend Developer",
+        company: "CodeBase",
+        location: "Bangalore",
+        type: "Internship"
+    },
+    {
+        title: "UI/UX Designer",
+        company: "DesignPro",
+        location: "Hyderabad",
+        type: "Full Time"
+    },
+    {
+        title: "Java Developer",
+        company: "TechSoft",
+        location: "Chennai",
+        type: "Internship"
+    },
+];
+
+const jobsContainer = document.getElementById("jobsContainer");
+
+function renderJobs(jobList) {
+    jobsContainer.innerHTML = "";
+
+    jobList.forEach(job => {
+        const jobCard = document.createElement("div");
+        jobCard.className = "job-card";
+
+        jobCard.innerHTML = `
+            <h3>${job.title}</h3>
+            <p>Company: ${job.company}</p>
+            <p>Location: ${job.location}</p>
+            <span>${job.type}</span>
+            `;
+
+        jobsContainer.appendChild(jobCard);
+    });
+}
+
+renderJobs(jobs);
